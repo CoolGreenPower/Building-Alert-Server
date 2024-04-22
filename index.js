d4cfec3114e17f42487028312c681eecc676abaf
CoolGreenPower <62005678+CoolGreenPower@users.noreply.github.com>
2024-04-21 22:15:19 -0400

    Updating index.js
    This script contains the API to fetch the data from MongoDB and pass it to frontend

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-// Define a schema for your data
+
 const dataSchema = new mongoose.Schema({
   alert_id: String,
   alert_desc: String,
@@ -16,25 +16,24 @@ const dataSchema = new mongoose.Schema({
   buildingId: mongoose.Schema.Types.ObjectId
 });
 
-// Create a model based on the schema
+
 const Data = mongoose.model('Data', dataSchema);
 
-// Connect to MongoDB
+
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri)
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Failed to connect to MongoDB', error));
 
-// Create an Express app
 const app = express();
-app.use(cors()); // Enable CORS for all routes
+app.use(cors()); 
 
-// Define a route to fetch data from MongoDB
+
 app.get('/api/data', async (req, res) => {
   try {
-    // Fetch data from MongoDB using Mongoose
+
     const data = await Data.find();
-    // Send the data as a response
+ 
     res.json(data);
   } catch (error) {
     console.error('Failed to fetch data', error);
@@ -42,7 +41,7 @@ app.get('/api/data', async (req, res) => {
   }
 });
 
-// Start the server
+
 const port = process.env.PORT || 3000; // Use environment variable for PORT or fallback to 3000
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
